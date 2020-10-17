3c613eba1abc19dc92272e5d0264cf3ef02a1660
Saif Sulaiman <saif.silver@gmail.com>
2020-10-17 20:14:00 +0530

    Webpack Prod and Development

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # webpack
+
+## Step 1 
+# Install Webpack and Setup
+
+## Step 2
+# Install babel compiler
+
--- webpack.config.js
+++ webpack.config.js
@@ -7,5 +7,7 @@ module.exports = {
     output: {
         path: path.join(__dirname, 'dist'),
         filename: 'app.bundle.js'
-    }
+    },
+
+    mode: process.env.NODE_ENV || 'development'
 }
