928b8c937e137c8bfc5d26f29b08cc0ce517e644
Saif Sulaiman <saif.silver@gmail.com>
2020-10-17 21:25:26 +0530

    HTMLWebPack Plugin Helps to Automatically Inject
    Js bundle generated by webpack to respective html file

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: path.join(__dirname, 'src', 'app.js'),
@@ -22,4 +23,10 @@ module.exports = {
   devServer: {
     contentBase: path.join(__dirname, 'src'),
   },
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.join(__dirname, 'src', 'index.html'),
+    }),
+  ],
 };
